1a58bb39b37a49c382a9cbe2c7ed6b8e8fffe2ad
Paweł Łęczycki <pawel@msd.info.pl>
2024-09-30 09:47:41 +0200

    Przygotowanie projektu

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using DAL;
+using Microsoft.EntityFrameworkCore;
+
+var config = new DbContextOptionsBuilder<Context>()
+    .UseSqlServer(@"Server=(local);Database=EFCore;Integrated security=true;TrustServerCertificate=true")
+    .Options;
+
+
+using ( var context = new Context(config))
+{
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+}
---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class Order : Entity
+    {
+        public string? Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
